0c7935a831244d8bc56e5ead5d65015ed8b97133
Whermerson19 <whermersonc@gmail.com>
2021-04-07 19:02:49 -0300

    class error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-import express from 'express'
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
 
-import appRouter from './routes'
+import { AppError } from "./errors/AppError";
+import appRouter from "./routes";
 
-const app = express()
-app.use(express.json())
+import "./database";
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
+      status: "error",
+      message: "internal server error",
+    });
+  }
+);
 
 app.use(appRouter);
-
-
